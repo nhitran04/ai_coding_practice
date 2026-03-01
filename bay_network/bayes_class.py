8bdf443536f7d130bd831841603000a1137c3885
Nhi <nhitran@terpmail.umd.edu>
2026-02-28 22:07:58 -0700

    implemented bayes network class for oop

---
+++ bay_network/bayes_class.py
@@ -0,0 +1,62 @@
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import VariableElimination
+from typing import Dict, List, Optional, Tuple
+from pgmpy.models import DiscreteBayesianNetwork
+
+
+class BayesNetworkClass:
+    """
+    Provides methods needed for Bayesian network.
+    """
+
+    @staticmethod
+    def initialize_network(
+        bayesNet: DiscreteBayesianNetwork,
+        nodes: List[str],
+        edges: List[Tuple[str, str]],
+    ) -> DiscreteBayesianNetwork:
+        """
+        Initializes the Bayesian Network with given nodes and edges.
+        """
+        for node in nodes:
+            bayesNet.add_node(node)
+
+        for parent, child in edges:
+            bayesNet.add_edge(parent, child)
+
+        return bayesNet
+
+    @staticmethod
+    def add_cpd_to_node(
+        bayesNet: DiscreteBayesianNetwork,
+        node: str,
+        values: List[List[float]],
+        evidence: Optional[List[str]],
+        evidence_card: Optional[List[int]],
+    ) -> TabularCPD:
+        """
+        Adds a CPD to the Bayesian Network.
+        """
+        cpd = TabularCPD(
+            node,
+            len(values),
+            values=values,
+            evidence=evidence,
+            evidence_card=evidence_card,
+        )
+        bayesNet.add_cpds(cpd)
+
+        return cpd
+
+    @staticmethod
+    def find_probability(
+        bayesNet: DiscreteBayesianNetwork,
+        variables: List[str],
+        evidence: Dict[str, int],
+    ) -> float:
+        """
+        Finds the probability of given variables with evidence.
+        """
+        solver = VariableElimination(bayesNet)
+        result = solver.query(variables=variables, evidence=evidence)
+        return result.values[1]
--- bay_network/burglary.py
+++ bay_network/burglary.py
@@ -5,125 +5,112 @@ but sometimes confuses the telephone ringing with the alarm. Mary, on the other 
 Given the evidence of who has or has not called, we would like to estimate the probability of a burglary.
 """
 
+from bayes_class import BayesNetworkClass
 from pgmpy.models import DiscreteBayesianNetwork
-from pgmpy.factors.discrete import TabularCPD
-from pgmpy.inference import VariableElimination
-from typing import Dict, List, Optional, Tuple
 
 
-def initialize_network(
-    nodes: List[str], edges: List[Tuple[str, str]]
-) -> DiscreteBayesianNetwork:
-    """
-    Initializes the Bayesian Network with given nodes and edges.
-    """
-    bayesNet = DiscreteBayesianNetwork()
+class BurglaryClass:
+    if __name__ == "__main__":
+        bayes_instance = BayesNetworkClass()
 
-    for node in nodes:
-        bayesNet.add_node(node)
+        # initialize nodes and edges list
+        nodes = ["burglary", "earthquake", "JohnCalls", "MaryCalls", "alarm"]
+        edges = [
+            ("burglary", "alarm"),
+            ("earthquake", "alarm"),
+            ("alarm", "JohnCalls"),
+            ("alarm", "MaryCalls"),
+        ]
 
-    for parent, child in edges:
-        bayesNet.add_edge(parent, child)
+        # create graph
+        bayesNet = DiscreteBayesianNetwork()
+        bayesNet = bayes_instance.initialize_network(bayesNet, nodes, edges)
 
-    return bayesNet
+        # add CPDs for each node
+        cpd_burglary = bayes_instance.add_cpd_to_node(
+            bayesNet, "burglary", [[0.999], [0.001]], None, None
+        )
+        cpd_earthquake = bayes_instance.add_cpd_to_node(
+            bayesNet, "earthquake", [[0.998], [0.002]], None, None
+        )
+        cpd_alarm = bayes_instance.add_cpd_to_node(
+            bayesNet,
+            "alarm",
+            [[0.999, 0.71, 0.06, 0.05], [0.001, 0.29, 0.94, 0.95]],
+            ["burglary", "earthquake"],
+            [2, 2],
+        )
+        cpd_john_calls = bayes_instance.add_cpd_to_node(
+            bayesNet, "JohnCalls", [[0.95, 0.10], [0.05, 0.90]], ["alarm"], [2]
+        )
+        cpd_mary_calls = bayes_instance.add_cpd_to_node(
+            bayesNet, "MaryCalls", [[0.99, 0.30], [0.01, 0.70]], ["alarm"], [2]
+        )
 
+        # check if model is correctly added
+        bayesNet.check_model()
+        print("Model is correct.")
 
-def add_cpd_to_node(
-    node: str,
-    values: List[List[float]],
-    evidence: Optional[List[str]],
-    evidence_card: Optional[List[int]],
-) -> TabularCPD:
-    """
-    Adds a CPD to the Bayesian Network.
-    """
-    cpd = TabularCPD(
-        node, len(values), values=values, evidence=evidence, evidence_card=evidence_card
-    )
-    bayesNet.add_cpds(cpd)
+        # probability there is a burglary
+        print("burglary", bayes_instance.find_probability(bayesNet, ["burglary"], None))
 
-    return cpd
+        # probability there is an earthquake
+        print(
+            "earthquake",
+            bayes_instance.find_probability(bayesNet, ["earthquake"], None),
+        )
 
+        # probability the alarm rings given that both a burglary and earthquake occur
+        print(
+            "alarm | burglary, earthquake",
+            bayes_instance.find_probability(
+                bayesNet, ["alarm"], {"burglary": 1, "earthquake": 1}
+            ),
+        )
 
-def find_probability(variables: List[str], evidence: Dict[str, int]) -> float:
-    """
-    Finds the probability of given variables with evidence.
-    """
-    solver = VariableElimination(bayesNet)
-    result = solver.query(variables=variables, evidence=evidence)
-    return result.values[1]
+        # probability the alarm rings given a burglary occurs but not an earthquake
+        print(
+            "alarm | burglary, ~earthquake",
+            bayes_instance.find_probability(
+                bayesNet, ["alarm"], {"burglary": 1, "earthquake": 0}
+            ),
+        )
 
+        # probability the alarm rings given an earthquake occurs but not a burglary
+        print(
+            "alarm | ~burglary, earthquake",
+            bayes_instance.find_probability(
+                bayesNet, ["alarm"], {"burglary": 0, "earthquake": 1}
+            ),
+        )
+        # probability the alarm rings given neither the earthquake nor burglary occur
+        print(
+            "alarm | ~burglary, ~earthquake",
+            bayes_instance.find_probability(
+                bayesNet, ["alarm"], {"burglary": 0, "earthquake": 0}
+            ),
+        )
 
-if __name__ == "__main__":
-    # initialize nodes and edges list
-    nodes = ["burglary", "earthquake", "JohnCalls", "MaryCalls", "alarm"]
-    edges = [
-        ("burglary", "alarm"),
-        ("earthquake", "alarm"),
-        ("alarm", "JohnCalls"),
-        ("alarm", "MaryCalls"),
-    ]
+        # probability that john calls given the alarm rings
+        print(
+            "JohnCalls | alarm",
+            bayes_instance.find_probability(bayesNet, ["JohnCalls"], {"alarm": 1}),
+        )
 
-    # create graph
-    bayesNet = initialize_network(nodes, edges)
+        # probability that john calls given the alarm does not ring
+        print(
+            "JohnCalls | ~alarm",
+            bayes_instance.find_probability(bayesNet, ["JohnCalls"], {"alarm": 0}),
+        )
 
-    # add CPDs for each node
-    cpd_burglary = add_cpd_to_node("burglary", [[0.999], [0.001]], None, None)
-    cpd_earthquake = add_cpd_to_node("earthquake", [[0.998], [0.002]], None, None)
-    cpd_alarm = add_cpd_to_node(
-        "alarm",
-        [[0.999, 0.71, 0.06, 0.05], [0.001, 0.29, 0.94, 0.95]],
-        ["burglary", "earthquake"],
-        [2, 2],
-    )
-    cpd_john_calls = add_cpd_to_node(
-        "JohnCalls", [[0.95, 0.10], [0.05, 0.90]], ["alarm"], [2]
-    )
-    cpd_mary_calls = add_cpd_to_node(
-        "MaryCalls", [[0.99, 0.30], [0.01, 0.70]], ["alarm"], [2]
-    )
+        # probability that mary calls given the alarm rings
+        print(
+            "MaryCalls | alarm",
+            bayes_instance.find_probability(bayesNet, ["MaryCalls"], {"alarm": 1}),
+        )
 
-    # check if model is correctly added
-    bayesNet.check_model()
-    print("Model is correct.")
-
-    # probability there is a burglary
-    print("burglary", find_probability(["burglary"], None))
-
-    # probability there is an earthquake
-    print("earthquake", find_probability(["earthquake"], None))
-
-    # probability the alarm rings given that both a burglary and earthquake occur
-    print(
-        "alarm | burglary, earthquake",
-        find_probability(["alarm"], {"burglary": 1, "earthquake": 1}),
-    )
-
-    # probability the alarm rings given a burglary occurs but not an earthquake
-    print(
-        "alarm | burglary, ~earthquake",
-        find_probability(["alarm"], {"burglary": 1, "earthquake": 0}),
-    )
-
-    # probability the alarm rings given an earthquake occurs but not a burglary
-    print(
-        "alarm | ~burglary, earthquake",
-        find_probability(["alarm"], {"burglary": 0, "earthquake": 1}),
-    )
-    # probability the alarm rings given neither the earthquake nor burglary occur
-    print(
-        "alarm | ~burglary, ~earthquake",
-        find_probability(["alarm"], {"burglary": 0, "earthquake": 0}),
-    )
-
-    # probability that john calls given the alarm rings
-    print("JohnCalls | alarm", find_probability(["JohnCalls"], {"alarm": 1}))
-
-    # probability that john calls given the alarm does not ring
-    print("JohnCalls | ~alarm", find_probability(["JohnCalls"], {"alarm": 0}))
-
-    # probability that mary calls given the alarm rings
-    print("MaryCalls | alarm", find_probability(["MaryCalls"], {"alarm": 1}))
-
-    # probability that mary calls given the alarm does not rings
-    print("MaryCalls | ~alarm", find_probability(["MaryCalls"], {"alarm": 0}))
+        # probability that mary calls given the alarm does not rings
+        print(
+            "MaryCalls | ~alarm",
+            bayes_instance.find_probability(bayesNet, ["MaryCalls"], {"alarm": 0}),
+        )
