b84c92953913f6fab765525102af2d688b45d142
Nhi <nhitran@terpmail.umd.edu>
2026-05-08 16:35:49 -0700

    added methods for variable elimination

--- bay_network/variable_elimination.py
+++ bay_network/variable_elimination.py
@@ -1,12 +1,28 @@
-'''
+"""
 The variable elimination algorithm for exact inference in Bayesian networks.
-'''
+"""
 
 
 def elimination_ask(X, e, bn):
-    '''
+    """
     Returns the probability distribution of the query variable X given the observed values e in the Bayesian network bn with variables vars.
-    '''
+    """
     factors = []
     for var in bn.vars:
         pass
+
+
+def order(vars):
+    pass
+
+
+def make_factor(var, e):
+    pass
+
+
+def pointwise_product(factors):
+    pass
+
+
+def normalize(product):
+    pass
