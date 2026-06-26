2d01cafd5866424915c4779d6842fdfbb85ffdcf
Nhi <nhitran@terpmail.umd.edu>
2026-06-25 18:01:21 -0700

    fixed env bug and updated get_states method

--- mdps/value_iteration.py
+++ mdps/value_iteration.py
@@ -37,64 +37,55 @@ class SimpleEnv(MiniGridEnv):
 
 
 class ValueIterationClass:
-    def __init__(self, reward=None):
+    def __init__(self, env, reward=None):
         if reward is None:
             reward = {0: -0.04, 1: 1.0, 2: -1.0, 3: np.nan}
 
-        env = SimpleEnv(render_mode="human")
+        self.env = env
         self.num_rows = env.height
         self.num_cols = env.width
-        self.num_states = len(self.get_states(env))
+        self.num_states = len(self.get_states())
         self.num_actions = 3
         self.reward = reward
-        self.reward_function = self.get_reward_function()
+        # self.reward_function = self.get_reward_function(env)
 
     def get_reward_function(self):
         reward_table = np.zeros(self.num_states)
         for row in range(self.num_rows):
             for col in range(self.num_cols):
-                pass
+                for action in range(self.num_actions):
+                    s = self.get_state_from_pos((row, col, action), self.env)
+                    reward_table[s] = self.reward[self.map[row, col, action]]
         return reward_table
 
-    def get_state_from_pos(self, pos, env):
-        indexed_states = self.get_indexed_states(env)
+    def get_state_from_pos(self, pos):
+        indexed_states = self.get_indexed_states(self.env)
         for i in indexed_states.keys():
             if i[0] == pos[0] and i[1] == pos[1] and i[2] == pos[2]:
                 return indexed_states[i]
 
-    def get_indexed_states(self, env):
-        states = self.get_states(env)
-        indexed_states = {}
-        index = 0
-        for i in range(self.num_states):
-            indexed_states[states[i]] = index
-            index += 1
-        return indexed_states
-
-    def get_states(self, env):
+    def get_states(self):
         """
         A state is represented by the location (x, y) and the agent's orientation.
-
-        Directions:
-        - 0: up
-        - 1: down
-        - 2: right
-        - 3: left
         """
-        states = []
+        indexed_states = {}
         directions = [0, 1, 2, 3]
-        for i in range(1, env.width - 1):
-            for j in range(1, env.height - 1):
-                cell = env.grid.get(i, j)
 
+        for i in range(1, self.env.width - 1):
+            for j in range(1, self.env.height - 1):
+                cell = self.env.grid.get(i, j)
                 if cell is not None and cell.type == "wall":
-                    continue
+                    indexed_states[(i, j)] = 3
+                elif cell is not None and cell.type == "goal":
+                    for d in directions:
+                        indexed_states[(i, j, d)] = 1
+                else:
+                    for d in directions:
+                        indexed_states[(i, j, d)] = 0
 
-                for d in directions:
-                    states.append((i, j, d))
-        return states
+        return indexed_states
 
-    def get_actions(self, env):
+    def get_actions(self):
         """
         Actions:
         - 0: left
@@ -102,7 +93,7 @@ class ValueIterationClass:
         - 2: forward
         """
         available_actions = []
-        for i in env.actions:
+        for i in self.env.actions:
             if i.value >= 0 and i.value <= 2:
                 available_actions.append(i.value)
         return available_actions
@@ -110,11 +101,11 @@ class ValueIterationClass:
 
 if __name__ == "__main__":
     env = SimpleEnv(render_mode="human")
-    value_iter = ValueIterationClass()
+    env.reset()
+    value_iter = ValueIterationClass(env)
     # manual_control = ManualControl(env, seed=42)
     # manual_control.start()
-    # print(get_states(env))
+    print(value_iter.get_states())
     # print(get_actions(env))
     # print(get_transition_model(env, random_rate=0.2))
-    # print(value_iter.get_indexed_states(env))
-    print(value_iter.get_state_from_pos((3, 1, 0), env))
+    # print(value_iter.get_state_from_pos((3, 1, 0), env))
